316585f6815596f2b3b6b3e6417c1ea5393c3de5
bvacaliuc <bvacaliuc@gmail.com>
2016-03-13 15:38:23 +0000

    keep trying...

--- setup.sh
+++ setup.sh
@@ -7,36 +7,31 @@ WRITABLE=/usr/local/var
 
 # Installing dependencies (from simple_ra/README)
 sudo pip install ephem
-mkdir $HOME/bin
+mkdir ${HOME}/bin
 echo "export PATH=\"$HOME/bin;${PATH}\"" >> $HOME/.bashrc
 echo "export PYTHONPATH=\"$HOME/bin;${PYTHONPATH}\"" >> $HOME/.bashrc
-source $HOME/.bashrc
+source ${HOME}/.bashrc
 # simple_ra/README says to use svn, but the repos has moved...
 ##svn co https://www.cgran.org/svn/projects/gr-ra_blocks
 (cd ${WRITABLE} ; git clone https://github.com/patchvonbraun/gr-ra_blocks.git)
-(cd gr-ra_blocks
-cd trunk
-cmake .
-make
-sudo make install
-sudo ldconfig)
+(cd ${WRITABLE}/gr-ra_blocks/trunk ; cmake . ; make ; sudo make install ; sudo ldconfig)
 
 # get simple_ra, build it
-(cd ${WRITABLE} ; git clone https://github.com/patchvonbraun/simple_ra)
+(cd ${WRITABLE} ; git clone https://github.com/patchvonbraun/simple_ra.git)
 (cd ${WRITABLE}/simple_ra ; sudo make install)
 # this puts stuff in $HOME/bin, but makes a few mistakes...
-cd $HOME/bin
+cd ${HOME}/bin
 sudo chown ubuntu:ubuntu simple_ra_receiver.py
 
 # give a hint...
 echo simple_ra is probably going to work...
 echo execute it from the command line by:
 echo
-echo cd $HOME/bin
+echo cd ${HOME}/bin
 echo sudo ./simple_ra --devid rtl=0,buflen=65536 --spde
 echo 
 echo and when you are running in spectral mode, be sure to press 'Autoscale'
-cd $HOME/bin
+cd ${HOME}/bin
 
 
 
