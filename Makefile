3bb9f2cb9a06277d3c46123fc3a1e7f14a76a7d7
bvacaliuc <bvacaliuc@gmail.com>
2016-03-13 15:15:20 +0000

    get rid of the symlink stuff...

--- Makefile
+++ Makefile
@@ -1,17 +1,7 @@
 #
 # Alternate way of running the various setup scripts
 
-all:	mount
+all:
 	chmod a+x ./setup.sh
 	sudo ./setup.sh
-
-# figure out where your writable file system is
-##WRITABLE=/media/ubuntu/2917309f-7599-4b7a-8fb6-1708ffc4050c/var
-WRITABLE=${HOME}/var
-
-mount:	${WRITABLE}
-	sudo ln -s ${WRITABLE} /usr/local/var 
-
-${WRITABLE}:
-	mkdir ${WRITABLE}
 
--- setup.sh
+++ setup.sh
@@ -2,16 +2,19 @@
 # NB: the below line is to create a mountable/writable filesystem to /usr/local/var
 #     the mount point will be different on every system/VM.  it must be world-writable/readable
 # ln -s /media/ubuntu/2917309f-7599-4b7a-8fb6-1708ffc4050c/var /usr/local/var
+# ugh.. its just a hassle.  Use $HOME/var, or whatever...
+WRITABLE=$HOME/var
+mkdir ${WRITABLE}
 
 # Installing dependencies (from simple_ra/README)
 sudo pip install ephem
 mkdir $HOME/bin
-echo "export \"PATH=$HOME/bin;${PATH}\"" >> $HOME/.bashrc
-echo "export \"PYTHONPATH=$HOME/bin;${PYTHONPATH}\"" >> $HOME/.bashrc
+echo "export PATH=\"$HOME/bin;${PATH}\"" >> $HOME/.bashrc
+echo "export PYTHONPATH=\"$HOME/bin;${PYTHONPATH}\"" >> $HOME/.bashrc
 source $HOME/.bashrc
 # simple_ra/README says to use svn, but the repos has moved...
 ##svn co https://www.cgran.org/svn/projects/gr-ra_blocks
-(cd /usr/local/var ; git clone https://github.com/patchvonbraun/gr-ra_blocks.git)
+(cd ${WRITABLE} ; git clone https://github.com/patchvonbraun/gr-ra_blocks.git)
 (cd gr-ra_blocks
 cd trunk
 cmake .
@@ -20,8 +23,8 @@ sudo make install
 sudo ldconfig)
 
 # get simple_ra, build it
-(cd /usr/local/var ; git clone https://github.com/patchvonbraun/simple_ra)
-(cd /usr/local/var/simple_ra ; sudo make install)
+(cd ${WRITABLE} ; git clone https://github.com/patchvonbraun/simple_ra)
+(cd ${WRITABLE}/simple_ra ; sudo make install)
 # this puts stuff in $HOME/bin, but makes a few mistakes...
 cd $HOME/bin
 sudo chown ubuntu:ubuntu simple_ra_receiver.py
