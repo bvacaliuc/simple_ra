9853e7204e69516cdef5c455b3cf783ae6737394
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2016-03-26 17:14:29 +0000

    cleanup user interaction prior to implementing persistent

--- persistent.sh
+++ persistent.sh
@@ -12,23 +12,31 @@ read udev
 dev=`df -h | grep $udev | tr -s ' ' | cut -f1 -d' '`
 mnt=`df -h | grep $udev | tr -s ' ' | cut -f6 -d' '`
 
+echo '\nSelected:'
 echo WRITABLE=${WRITABLE}
 echo dev=${dev}
 echo mnt=${mnt}
 echo udev=${udev}
 
-if [ -z ${dev} -o -z ${mnt} ] ; then
-	echo 'your choice did not match an existing device or mount'
+if [ -z "${dev}" -o -z "${mnt}" ] ; then
+	echo '\nYour choice did not match an existing device or mount'
 	echo 'try again...'
 	exit 1
 fi
-if [ ! -b ${dev} -o ! -d ${mnt} ] ; then
-	echo 'either dev is not a device or mnt is not a folder'
+if [ ! -b "${dev}" -o ! -d "${mnt}" ] ; then
+	echo '\nEither dev is not a device or mnt is not a folder'
 	echo 'try again...'
+	exit 1
+fi
+echo '\nUsing:'
+if ! (echo "${mnt}" | grep media) ; then
+	echo '\nThe device you selected is not a media device.'
+	echo 'This will not likely have the effect you want and'
+	echo 'I am too cowardly to go on, so please try again...'
 	exit 1
 fi
 
-echo "Initializing ${dev} for persistent storage"
+echo "\nInitializing ${dev} for persistent storage"
 echo '*** LAST chance to stop before wiping the disk ***'
 echo ''
 echo 'Are you sure you are ready to do this?  You must type "yes"'
@@ -36,6 +44,10 @@ read res
 if [ ! ${res} = "yes" ] ; then
 	exit 1
 fi
+
+# TEMPORARY: remove
+echo "*** next steps are to unmount/partition and create the filesystem"
+echo "    but they have yet to be implemented"
 
 # unmount the disk
 
