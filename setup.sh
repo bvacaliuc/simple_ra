dba3a89e8ca2438e5c6b0f02ee5de4824e9851ca
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2016-08-09 01:47:02 +0000

    fix crufty comments; make simple_ra as user, remove sudo from exec command

--- setup.sh
+++ setup.sh
@@ -114,23 +114,19 @@ echo "3d6c19d92a2a80fef87770f3e2007453 pyephem-3.7.6.0.tar.gz" > ${WRITABLE}/pye
 (cd ${WRITABLE}/pyephem ; wget https://pypi.python.org/packages/source/p/pyephem/pyephem-3.7.6.0.tar.gz ; md5sum -c md5sum.txt )
 sudo -H pip install ${WRITABLE}/pyephem/pyephem-3.7.6.0.tar.gz
 
-# simple_ra requires gawk (and GNU Radio Live DVD installs 4.1.0 using apt-get)
+# simple_ra requires gawk (and GNU Radio Live DVD installs 4.0.1 using apt-get)
 mkdir -p ${WRITABLE}/gawk
 echo "bab2bda483e9f32be65b43b8dab39fa5 gawk-4.0.1.tar.gz" > ${WRITABLE}/gawk/md5sum.txt
 (cd ${WRITABLE}/gawk ; wget http://ftp.gnu.org/gnu/gawk/gawk-4.0.1.tar.gz ; md5sum -c md5sum.txt )
 (cd /tmp ; tar xf ${WRITABLE}/gawk/gawk-4.0.1.tar.gz ; cd gawk-4.0.1 ; ./configure ; make ; sudo make install )
 
-# simple_ra/README says to use svn, but the repos has moved...
-##svn co https://www.cgran.org/svn/projects/gr-ra_blocks
+# simple_ra requires gr-ra_blocks
 (cd ${WRITABLE} ; git clone https://github.com/patchvonbraun/gr-ra_blocks.git)
 (cd ${WRITABLE}/gr-ra_blocks ; cmake . ; make ; sudo make install ; sudo ldconfig)
 
 # get simple_ra, build it
 (cd ${WRITABLE} ; git clone https://github.com/patchvonbraun/simple_ra.git)
-(cd ${WRITABLE}/simple_ra ; sudo make install)
-# this puts stuff in $HOME/bin, but makes a few mistakes...
-#cd ${HOME}/bin
-#sudo chown ubuntu:ubuntu simple_ra_receiver.py
+(cd ${WRITABLE}/simple_ra ; make ; sudo make install)
 
 if [ ! -x ${HOME}/bin/simple_ra ] ; then
 	echo "Bummer.  simple_ra did not get built where I expected it."
@@ -146,10 +142,10 @@ echo simple_ra is probably going to work...
 echo execute it from the command line by:
 echo
 echo cd ${HOME}/bin
-echo sudo ./simple_ra --devid rtl=0,offset_tune=1
+echo ./simple_ra --devid rtl=0
 echo 
 echo and when you are running in spectral mode, be sure to press 'Autoscale'
-#cd ${HOME}/bin
+
 
 
 
